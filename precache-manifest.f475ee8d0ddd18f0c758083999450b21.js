745aecf415e09ceb774dbe79e57c8c438d280164
balajinikhil <balajiedward007@gmail.com>
2020-07-07 22:21:46 +0530

    Updates

---
+++ precache-manifest.f475ee8d0ddd18f0c758083999450b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec47d8db8e2d85ae76cf64dd04d5df5",
+    "url": "/season-react/index.html"
+  },
+  {
+    "revision": "003d175856ed21791dc9",
+    "url": "/season-react/static/css/main.6ba1b488.chunk.css"
+  },
+  {
+    "revision": "6ae538beb5b6f353f0d9",
+    "url": "/season-react/static/js/2.7ecb5860.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/season-react/static/js/2.7ecb5860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003d175856ed21791dc9",
+    "url": "/season-react/static/js/main.ccb4e03b.chunk.js"
+  },
+  {
+    "revision": "57efa428a86a52bacfee",
+    "url": "/season-react/static/js/runtime-main.81d562d5.js"
+  }
+]);
